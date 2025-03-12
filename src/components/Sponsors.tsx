17d557512bd4615df12b5387558614fa0692bc51
LoneTerrorX <chakrabortyprasun10@gmail.com>
2025-03-12 20:59:56 +0530

    Sponsor Animation Mobile View fixes

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Sponsors() {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    threshold: 0.1
-  });
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const containerRef = useRef(null);
+  const [containerWidth, setContainerWidth] = useState(0);
 
   const sponsors = [
     'https://i.postimg.cc/RFwR3PdT/oil-india.jpg',
@@ -16,10 +16,15 @@ export default function Sponsors() {
     'https://i.postimg.cc/vHbPndpx/meal-by-box.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh0t_MVuJUkDmYRIHdFALAzd_spGi72DUmsQ&s',
     'https://i.postimg.cc/8cPpYzyf/decathlon.webp',
-    'https://i.postimg.cc/sx8xMBBK/raju-reyaz.jpg',
     'https://images.unsplash.com/vector-1741113755107-5fa5aa1b8da0?q=80&w=2440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
+    'https://i.postimg.cc/sx8xMBBK/raju-reyaz.jpg',
   ];
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setContainerWidth(containerRef.current.scrollWidth / 2);
+    }
+  }, []);
 
   return (
     <div className="py-16 bg-gray-800" id="sponsors">
@@ -37,19 +42,20 @@ export default function Sponsors() {
 
         <div className="relative overflow-hidden">
           <motion.div
+            ref={containerRef}
             className="flex"
-            initial={{ x: "100%" }}
-            animate={{ x: "-100%" }}
+            initial={{ x: 500 }}
+            animate={{ x: -containerWidth }}
             transition={{
               ease: "linear",
-              duration: 20,
-              repeat: Infinity
+              duration: 12,
+              repeat: Infinity,
             }}
           >
             {[...sponsors, ...sponsors].map((logo, index) => (
               <div
                 key={index}
-                className="flex-none w-48 h-48 mx-4 bg-white rounded-lg p-4 flex items-center justify-center"
+                className="flex-none w-32 md:w-48 h-32 md:h-48 mx-4 bg-white rounded-lg p-4 flex items-center justify-center"
               >
                 <img
                   src={logo}
